db9dbaf66c5be91228a17813f9ebd516ccd28dcc
ErickVasquez11 <00065520@uca.edu.sv>
2023-05-01 18:56:03 -0600

    :chart_with_upwards_trend: Adding Request Error and aplication proteties and interfaces

--- src/main/java/com/erickvasquez/documentos/controllers/LibraryController.java
+++ src/main/java/com/erickvasquez/documentos/controllers/LibraryController.java
@@ -5,11 +5,17 @@ import java.util.Calendar;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.erickvasquez.documentos.models.dtos.SaveBookDTO;
+import com.erickvasquez.documentos.models.entities.Book;
+import com.erickvasquez.documentos.services.BookService;
+import com.erickvasquez.documentos.utils.RequestErrorHandler;
 
 import jakarta.validation.Valid;
 
